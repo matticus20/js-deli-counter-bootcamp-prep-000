9880d45b35416055666ea01e4845401f3cfd28d1
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-02-02 05:41:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function currentLine(katzDeliLine) {
   var newArray = []
   if (katzDeliLine.length > 0) {
     for (let i = 0; i < katzDeliLine.length; i++) {
-      newArray.push(`${i+1}. ${katzDeliLine[i]} `)
+      newArray.push(`${i+1}. ${katzDeliLine[i]}`)
     }
-    return `The line is currently: ${newArray}`
+    return `The line is currently: ${newArray} `
   } else {
     return "The line is currently empty."
   }

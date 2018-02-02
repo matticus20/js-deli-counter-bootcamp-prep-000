23acf18b13916aaac74b444a7c387ea1bddc3233
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-02-02 05:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ function takeANumber(katzDeliLine, newName) {
 }
 
 function nowServing(katzDeliLine) {
-  
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`
+  } else {
+    return "There is nobody waiting to be served."
+  }
 }

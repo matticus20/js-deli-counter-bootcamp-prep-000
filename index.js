318c2d41ee291da0bc991113baa18f00fba2c558
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-02-02 05:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ var katzDeliLine = []
 
 function takeANumber(katzDeliLine, newName) {
   katzDeliLine.push(name)
-  return `Welcome ${newName}. You are number ${katzDeliLine.length} in line.`
+  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine.shift()}.`
   } else {
-    return "There is nobody waiting to be served."
+    return "There is nobody waiting to be served!"
   }
 }
 
 function currentLine(katzDeliLine) {
   var newArray = []
-  if (katzDeliLength.length > 0) {
+  if (katzDeliLine.length > 0) {
     for (let i = 0; i < katzDeliLine.length; i++) {
       newArray = [`${i+1}. ${newName}`]
     }
